c4a2c6f8795e75cd305d2950a8f591fce07f4d15
vkodithala <varoon.kodithala@gmail.com>
2023-09-28 23:47:36 -0400

    tailwindcss setup

--- nextjs-app/src/App.js
+++ nextjs-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <body>
+
+      <div class="flex flex-row bg-gray-800 px-8 py-7 text-slate-50">
+        <div class="basis-1/6 font-semibold">Experimental Flights VIP</div>
+        <div class="basis-1/4"><button class="hover:text-sky-300 ease-in duration-200 font-semibold">Create New</button></div>
+        <div class="basis-1/4"></div>
+      </div>
+    </body>
+    </>
   );
 }
 
