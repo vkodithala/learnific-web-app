006caac686b40f7fcec4a5260fae574abf6a98e3
Arnav Patidar <arnav.patidar@gmail.com>
2023-12-17 18:37:36 -0500

    initial state machine for onboarding

--- client/src/Onboarding.js
+++ client/src/Onboarding.js
@@ -1,93 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './OnboardingPage.css';
 
+const ConversationState = {
+    WELCOME: 'welcome',
+    AREAS: 'areas',
+    TOPICS: 'topics',
+    WHY_TOPICS: 'why_topics',
+    EXPERTISE: 'expertise',
+    SCOPE: 'scope',
+    FREQUENCY: 'frequency',
+    OTHER: 'other'
+};
 const OnboardingPage = () => {
-    const [messages, setMessages] = useState([]);
+    const welcomeMessage = {
+        text: "Hi there! I'm Avery. Welcome to Research Digest. I'd love to learn about your interests so I can put together a personalized newsletter for you.",
+        sender: 'bot',
+        username: 'Avery - Content Curator',
+        iconUrl: '/contentCurator.jpeg'
+    };
+
+    const [messages, setMessages] = useState([welcomeMessage]);
     const [input, setInput] = useState('');
     const [isBotTyping, setIsBotTyping] = useState(false);
-    const [userField, setUserField] = useState('');
-    const [userTopics, setUserTopics] = useState([]);
+    const [conversationState, setConversationState] = useState(ConversationState.AREAS);
+    const [userInterests, setUserInterests] = useState({
+        researchAreas: [],
+        specificTopics: [],
+        whyTopics: '', //text response
+        expertise: '', //make this number 1-5?
+        detailed: '', //in-depth or broad
+        frequency: '' //daily or weekly
+    });
 
-    useEffect(() => {
-        sendWelcomeMessage();
-    }, []);
-
-    const sendWelcomeMessage = () => {
-        const welcomeMessage = {
-            text: "Hi there! I'm Avery. Welcome to Research Digest. I'd love to learn about your interests so our team can put together a personalized newsletter for you.",
-            sender: 'bot',
-            username: 'Avery - Content Curator',
-            iconUrl: '/contentCurator.jpeg'
-        };
-        setMessages([welcomeMessage]);
-    };
-
-    const sendFieldInquiry = () => {
-        const fieldInquiry = {
-            text: "First off, what is ONE general area of research that excites you? You know, like healthcare, physics, AI - whatever gets you excited. I want to know what you're passionate about.",
-            sender: 'bot',
-            username: 'Avery - Content Curator',
-            iconUrl: '/contentCurator.jpeg'
-        };
-        setMessages((currentMessages) => [...currentMessages, fieldInquiry]);
-    };
-
-    const sendTopicInquiry = () => {
-        const topicInquiry = {
-            text: `Okay, now thinking specifically about ${userField}, what is ONE topic in that area really captures your curiosity? For example...[give examples]. `,
-            sender: 'bot',
-            username: 'Avery - Content Curator',
-            iconUrl: '/contentCurator.jpeg'
-        };
-        setMessages((currentMessages) => [...currentMessages, topicInquiry]);
-    };
-
-    const askWhyTopic = () => {
-        const latestTopic = userTopics[userTopics.length - 1];
-    
-        const whyTopicMessage = {
-            text: `Tell me a bit more about why ${latestTopic} fascinates you. The more details the better so I can fine tune your newsletter!`,
-            sender: 'bot',
-            username: 'Avery - Content Curator',
-            iconUrl: '/contentCurator.jpeg'
-        };
-        setMessages((currentMessages) => [...currentMessages, whyTopicMessage]);
-    };
-    
-
-    const askMoreTopics = () => {
-        // Create a unique list of topics within the current field
-        const uniqueTopics = Array.from(new Set(userTopics));
-    
-        const moreTopicsMessage = {
-            text: `That sounds awesome! Are there any more ${userField} topics like ${uniqueTopics.join(', ')} that you’re interested in?`,
-            sender: 'bot',
-            username: 'Avery - Content Curator',
-            iconUrl: '/contentCurator.jpeg'
-        };
-        setMessages((currentMessages) => [...currentMessages, moreTopicsMessage]);
-    };
-    
-
-    const askExpertise = () => {
-        const expertiseMessage = {
-            text: `Let me ask - would you consider yourself pretty knowledgeable about any of the topics we just discussed? ${userTopics.join(', ')}?`,
-            sender: 'bot',
-            username: 'Avery - Content Curator',
-            iconUrl: '/contentCurator.jpeg'
-        };
-        setMessages((currentMessages) => [...currentMessages, expertiseMessage]);
-    };
-
-    const askNewTopics = () => {
-        const newTopicsMessage = {
-            text: `Also, are any of these topics totally new territory for you? Areas where you feel like a complete beginner and are really just looking to learn the basics? ${userTopics.join(', ')}?`,
-            sender: 'bot',
-            username: 'Avery - Content Curator',
-            iconUrl: '/contentCurator.jpeg'
-        };
-        setMessages((currentMessages) => [...currentMessages, newTopicsMessage]);
-    };
+    const botMessage = (text) => ({
+        text,
+        sender: 'bot',
+        username: 'Avery - Content Curator',
+        iconUrl: '/contentCurator.jpeg'
+    });
 
     const userMessage = (input) => ({
         text: input,
@@ -99,45 +49,63 @@ const OnboardingPage = () => {
     const sendMessage = (event) => {
         event.preventDefault();
         if (input.trim() !== '') {
-            const newUserMessage = userMessage(input);
-            setMessages([...messages, newUserMessage]);
-
+            setMessages([...messages, userMessage(input)]);
+            handleConversation(input);
             setInput('');
-            setIsBotTyping(true);
-
-            setTimeout(() => {
-                setIsBotTyping(false);
-
-                // Determine the next message based on the conversation stage
-                switch (messages.length) {
-                    case 1:
-                        sendFieldInquiry();
-                        break;
-                    case 3:
-                        setUserField(input.trim());
-                        sendTopicInquiry();
-                        break;
-                    case 5:
-                        setUserTopics([...userTopics, input.trim()]);
-                        askWhyTopic();
-                        break;
-                    case 7:
-                        askMoreTopics();
-                        break;
-                    case 9:
-                        if (input.trim().toLowerCase() === 'yes') {
-                            sendTopicInquiry(); // Repeat the topic inquiry
-                        } else {
-                            askExpertise();
-                        }
-                        break;
-                    case 11:
-                        askNewTopics();
-                        break;
-                    // Add more cases as needed for further interactions
-                }
-            }, 3000);
         }
+    };
+
+    const handleConversation = (userInput) => {
+        setIsBotTyping(true);
+
+        setTimeout(() => {
+            setIsBotTyping(false);
+            let nextMessageText = '';
+
+            switch (conversationState) {
+                case ConversationState.AREAS:
+                    nextMessageText = "First off, what general areas of research light you up? You know, like healthcare, physics, AI - whatever gets you excited. I want to know what you're passionate about.";
+                    setConversationState(ConversationState.TOPICS);
+                    break;
+                case ConversationState.TOPICS:
+                    const areas = userInput.split(',').map(area => area.trim());
+                    setUserInterests({ ...userInterests, researchAreas: areas });
+                    nextMessageText = `Okay, now thinking specifically about ${areas.join(', ')}, what topics in these areas really capture your curiosity?`;
+                    setConversationState(ConversationState.WHY_TOPICS);
+                    break;
+                case ConversationState.WHY_TOPICS:
+                    const topics = userInput.split(',').map(topic => topic.trim());
+                    setUserInterests({ ...userInterests, specificTopics: topics });
+                    nextMessageText = `Tell me a bit more about why ${topics.join(', ')} fascinate you. The more details the better so I can fine tune your newsletter!`;
+                    setConversationState(ConversationState.EXPERTISE);
+                    break;
+                case ConversationState.EXPERTISE:
+                    setUserInterests({ ...userInterests, whyTopics: userInput });
+                    nextMessageText = "Let me ask - would you consider yourself pretty knowledgeable about any of the topics we just discussed? I want to get a sense of where you are starting from in terms of background knowledge.";
+                    setConversationState(ConversationState.SCOPE);
+                    break;
+                case ConversationState.SCOPE:
+                    setUserInterests({ ...userInterests, expertise: userInput.toLowerCase() });
+                    nextMessageText = "And when you're learning about this stuff, are you looking to go super in-depth? Or do you prefer a more general overview to start with?";
+                    setConversationState(ConversationState.FREQUENCY);
+                    break;
+                case ConversationState.FREQUENCY:
+                    setUserInterests({ ...userInterests, detailed: userInput.toLowerCase() });
+                    nextMessageText = `Great! We'll send you personalized updates about ${areas.join(', ')} right to your inbox. How often would you like us to send you The Research Digest?`
+                    setConversationState(ConversationState.OTHER);
+                    break;
+                case ConversationState.OTHER:
+                    setUserInterests({ ...userInterests, frequency: userInput.toLowerCase() });
+                    nextMessageText = "Let me know if you have any other requests for the type of content you want to see. I'm here to make this newsletter perfect for you! Anything else on your mind?";
+                    setConversationState(ConversationState.WELCOME);
+                    break;
+                    
+                default:
+                    nextMessageText = "I'm not sure how to respond to that. Could you please repeat?";
+            }
+
+            setMessages(currentMessages => [...currentMessages, botMessage(nextMessageText)]);
+        }, 3000);
     };
 
     return (
@@ -179,6 +147,8 @@ const OnboardingPage = () => {
         </div>
     );
 };
+
+
 
 const styles = {
     container: {
