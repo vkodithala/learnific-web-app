13e7a7cf663c113dca0c40427485ae098bdfebde
vkodithala <varoon.kodithala@gmail.com>
2023-12-18 22:21:25 -0600

    backend fixes

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
-import { useEffect, useState } from 'react';
-import Home from './Home'
-import OnboardingPage from './Onboarding';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
