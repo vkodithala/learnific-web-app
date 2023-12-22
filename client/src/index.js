4b1ee5145085d15542a4bb20a0aba0b90fc8a5e9
Arnav Patidar <arnav.patidar@gmail.com>
2023-12-21 23:06:24 -0500

    Made it app

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,6 @@ import { SignUpBar } from "./components/SignUpBar";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignUpBar />
+    <App />
   </React.StrictMode>
 );
