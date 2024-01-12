f52bce9b4f8762d5b7c7b69412b980b75466c251
Arnav Patidar <arnav.patidar@gmail.com>
2024-01-11 21:22:38 -0500

    Update Area1.jsx

--- client/src/components/onboarding/Area1.jsx
+++ client/src/components/onboarding/Area1.jsx
@@ -14,7 +14,7 @@ export default function Area1() {
     <div className="flex flex-col ">
       <div className="w-full mx-2 flex-1">
         <div className="font-bold mt-3 text-gray-500 text-xs uppercase">
-          What subtopics within <span className="text-2xl">{userData['area1']}</span> draw your attention?
+          What subtopics within <span className="font-extrabold">{userData['area1']}</span> draw your attention?
         </div>
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
         <input
